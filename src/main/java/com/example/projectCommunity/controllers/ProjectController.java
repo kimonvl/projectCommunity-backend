b5f47d9f21a99b067568f3286fc8219e73551e49
kimonvl <kimonvloutis@gmail.com>
2025-12-10 20:56:38 +0200

    fix: Refactor controllers and services: Project

--- src/main/java/com/example/projectCommunity/controllers/ProjectController.java
+++ src/main/java/com/example/projectCommunity/controllers/ProjectController.java
@@ -5,8 +5,10 @@ import com.example.projectCommunity.DTOs.requests.CreateProjectRequest;
 import com.example.projectCommunity.DTOs.requests.ProjectInvitationRequest;
 import com.example.projectCommunity.DTOs.response.ProjectDTO;
 import com.example.projectCommunity.DTOs.response.ResponseDTO;
+import com.example.projectCommunity.controllers.controllerUtils.ResponseFactory;
 import com.example.projectCommunity.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,26 +23,26 @@ public class ProjectController {
 
     @PostMapping("/create")
     public ResponseEntity<ResponseDTO<ProjectDTO>> createProject(@RequestBody CreateProjectRequest createProjectRequest, Principal principal) {
-        return projectService.createProject(createProjectRequest, principal.getName());
+        return ResponseFactory.createSuccessResponse(projectService.createProject(createProjectRequest, principal.getName()), "Project created", HttpStatus.CREATED);
     }
 
     @GetMapping("/myProjects")
     public ResponseEntity<ResponseDTO<List<ProjectDTO>>> getMyProjects(Principal principal) {
-        return projectService.getMyProjects(principal.getName());
+        return ResponseFactory.createSuccessResponse(projectService.getMyProjects(principal.getName()), "Projects fetched", HttpStatus.ACCEPTED);
     }
 
     @GetMapping("/selectedProject/{projectId}")
     public ResponseEntity<ResponseDTO<ProjectDTO>> getSelectedProject(@PathVariable long projectId, Principal principal) {
-        return projectService.getSelectedProject(projectId, principal.getName());
+        return ResponseFactory.createSuccessResponse(projectService.getSelectedProject(projectId, principal.getName()), "Project fetched", HttpStatus.ACCEPTED);
     }
 
     @PostMapping("/sendProjectInvitation")
     ResponseEntity<ResponseDTO<Object>> sendProjectInvitation(@RequestBody ProjectInvitationRequest projectInvitationRequest, Principal principal) {
-        return projectService.sendProjectInvitation(projectInvitationRequest.getReceiverEmails(), projectInvitationRequest.getProjectId(), principal.getName());
+        return ResponseFactory.createSuccessResponse(null, "Project invitation sent", HttpStatus.CREATED);
     }
 
     @PostMapping("/acceptInvitation")
     public ResponseEntity<ResponseDTO<ProjectDTO>> acceptProjectInvitation(@RequestBody AcceptProjectInvitationRequest acceptProjectInvitationRequest, Principal principal) {
-        return projectService.acceptProjectInvitation(principal.getName(), acceptProjectInvitationRequest.getProjectId(), acceptProjectInvitationRequest.getNotificationId());
+        return ResponseFactory.createSuccessResponse(projectService.acceptProjectInvitation(principal.getName(), acceptProjectInvitationRequest.getProjectId(), acceptProjectInvitationRequest.getNotificationId()), "Project invitation accepted", HttpStatus.CREATED);
     }
 }
--- src/main/java/com/example/projectCommunity/services/ProjectServiceImpl.java
+++ src/main/java/com/example/projectCommunity/services/ProjectServiceImpl.java
@@ -3,12 +3,15 @@ package com.example.projectCommunity.services;
 import com.example.projectCommunity.DTOs.requests.CreateProjectRequest;
 import com.example.projectCommunity.DTOs.response.ProjectDTO;
 import com.example.projectCommunity.DTOs.response.ResponseDTO;
+import com.example.projectCommunity.exceptions.ProjectNotFoundException;
+import com.example.projectCommunity.exceptions.UserNotFoundException;
 import com.example.projectCommunity.mappers.ProjectMapper;
 import com.example.projectCommunity.models.chat.Chat;
 import com.example.projectCommunity.models.project.Project;
 import com.example.projectCommunity.models.user.User;
 import com.example.projectCommunity.repos.ProjectRepo;
 import com.example.projectCommunity.repos.UserRepo;
+import com.example.projectCommunity.services.serviceUtils.ServiceUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -29,11 +32,10 @@ public class ProjectServiceImpl implements ProjectService{
     private NotificationService notificationService;
 
     @Override
-    public ResponseEntity<ResponseDTO<ProjectDTO>> createProject(CreateProjectRequest createProjectRequest, String email) {
+    public ProjectDTO createProject(CreateProjectRequest createProjectRequest, String email) {
         User user = userRepo.findByEmail(email);
-        ProjectDTO projectDTO = new ProjectDTO();
         if (user == null){
-            return new ResponseEntity<>(new ResponseDTO<>(null, "User doesn't exist", false), HttpStatus.BAD_REQUEST);
+            throw new UserNotFoundException("User not found");
         }
 
         Project project = new Project();
@@ -49,67 +51,54 @@ public class ProjectServiceImpl implements ProjectService{
         project.setChat(chat);
         Project savedProject = projectRepo.save(project);
         //convert to dto with mapstruct
-        projectDTO = projectMapper.toDto(savedProject);
-        return new ResponseEntity<>(new ResponseDTO<>(projectDTO, "Project created", true), HttpStatus.CREATED);
+        return projectMapper.toDto(savedProject);
     }
 
     @Override
-    public ResponseEntity<ResponseDTO<List<ProjectDTO>>> getMyProjects(String email) {
+    public List<ProjectDTO> getMyProjects(String email) {
         List<Project> projects = projectRepo.findByParticipantsEmail(email);
-        List<ProjectDTO> projectDTOS = projectMapper.toDtoList(projects);
-        return new ResponseEntity<>(new ResponseDTO<>(projectDTOS, "Projects fetched", true), HttpStatus.ACCEPTED);
+        return projectMapper.toDtoList(projects);
     }
 
     @Override
-    public ResponseEntity<ResponseDTO<ProjectDTO>> getSelectedProject(long projectId, String email) {
+    public ProjectDTO getSelectedProject(long projectId, String email) {
         User user = userRepo.findByEmail(email);
         Project project;
-        ProjectDTO projectDTO;
         Optional<Project> projectOpt = projectRepo.findById(projectId);
         if (projectOpt.isEmpty()) {
-            return new ResponseEntity<>(new ResponseDTO<>(null, "Project not found", false), HttpStatus.BAD_REQUEST);
+            throw new ProjectNotFoundException("Project not found");
         }
         project = projectOpt.get();
-        if (!project.getParticipants().contains(user)) {
-            return new ResponseEntity<>(new ResponseDTO<>(null, "User doesn't have access to this project", false), HttpStatus.BAD_REQUEST);
-        }
-        projectDTO = projectMapper.toDto(project);
-        return new ResponseEntity<>(new ResponseDTO<>(projectDTO, "Project found", true), HttpStatus.ACCEPTED);
+        ServiceUtils.checkAccessToProject(projectRepo,project.getId(), user.getEmail(), "User doesn't have access to project");
+        return projectMapper.toDto(project);
     }
 
     @Override
-    public ResponseEntity<ResponseDTO<ProjectDTO>> acceptProjectInvitation(String email, long projectId, long notificationId) {
+    public ProjectDTO acceptProjectInvitation(String email, long projectId, long notificationId) {
         User user = userRepo.findByEmail(email);
         Optional<Project> projectOpt = projectRepo.findById(projectId);
         Project project;
         ProjectDTO projectDTO;
         if(projectOpt.isEmpty()) {
-            return new ResponseEntity<>(new ResponseDTO<>(null, "Project not found", false), HttpStatus.BAD_REQUEST);
+            throw new ProjectNotFoundException("Project not found");
         }
-        try {
-            notificationService.markAsSeen(notificationId);
-        } catch (Exception e) {
-            return new ResponseEntity<>(new ResponseDTO<>(null, e.getMessage(), false), HttpStatus.BAD_REQUEST);
-        }
+        notificationService.markAsSeen(notificationId);
+
         project = projectOpt.get();
         project.getParticipants().add(user);
-        projectDTO = projectMapper.toDto(projectRepo.save(project));
-
-        return new ResponseEntity<>(new ResponseDTO<>(projectDTO, "Joined to project: " + project.getTitle(), true), HttpStatus.ACCEPTED);
+        return projectMapper.toDto(projectRepo.save(project));
     }
 
     @Override
-    public ResponseEntity<ResponseDTO<Object>> sendProjectInvitation(List<String> receiverEmails, long projectId, String email) {
+    public void sendProjectInvitation(List<String> receiverEmails, long projectId, String email) {
         User sender = userRepo.findByEmail(email);
         List<User> receivers = userRepo.findByEmailIn(receiverEmails);
         Optional<Project> projectOpt = projectRepo.findById(projectId);
         Project project;
         if (projectOpt.isEmpty()) {
-            return new ResponseEntity<>(new ResponseDTO<>(null, "Project not found", false), HttpStatus.BAD_REQUEST);
+            throw new ProjectNotFoundException("Project not found");
         }
         project = projectOpt.get();
-        System.out.println("send project invitation project service");
         notificationService.sendProjectInviteNotification(receivers, sender, project);
-        return new ResponseEntity<>(new ResponseDTO<>(null, "Invitation sent", true), HttpStatus.CREATED);
     }
 }
